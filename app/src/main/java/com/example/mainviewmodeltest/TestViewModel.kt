e47346661ce8540c3483a63eacf8da4c6a73ef2c
sasi kumar <sasi.kumar@whitehatjr.com>
2022-04-22 13:46:00 +0530

    first commit

--- app/src/main/java/com/example/mainviewmodeltest/TestViewModel.kt
+++ app/src/main/java/com/example/mainviewmodeltest/TestViewModel.kt
@@ -7,11 +7,6 @@ import androidx.lifecycle.ViewModel
 class TestViewModel(private val initial : Int) : ViewModel() {
     var number : MutableLiveData<Int> = MutableLiveData(initial)
 
-    /*fun getNumber() : LiveData<Int> {
-        val liveData: LiveData<Int> = MutableLiveData(number)
-        return liveData
-    }*/
-
     fun add(int: Int) =number.value?.let{
         number.setValue(it+int)
 
